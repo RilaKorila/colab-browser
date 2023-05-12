bcccd2d466c8b4e3eea3cc9a4c111cd4e04db1e8
RilaKorila <murakami.ayana3@gmail.com>
2023-05-12 09:45:49 +0900

    nextauthでのログイン画面を簡易実装

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,23 @@
 import "../styles/globals.css";
 // import { getAuth } from "firebase/auth";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 // import { useEffect } from "react";
 // import { FirebaseAuthProvider } from "../libs/firebase/utils/auth";
 // import { signIn } from "libs/firebase/hooks";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   // const auth = getAuth();
   // useEffect(() => {
   //   signIn();
   // }, [auth]);
   return (
     <>
-      {/* <FirebaseAuthProvider> */}
-      <Component {...pageProps} />
-      {/* </FirebaseAuthProvider> */}
+      <SessionProvider session={session}>
+        {/* <FirebaseAuthProvider> */}
+        <Component {...pageProps} />
+        {/* </FirebaseAuthProvider> */}
+      </SessionProvider>
     </>
   );
 }
